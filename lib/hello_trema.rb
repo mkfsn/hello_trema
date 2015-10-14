451492359d092c1f38e3bb299def33af7201e40a
mkfsn <p408865@gmail.com>
2015-10-14 14:24:29 +0900

    Modify log messages.
    + Show class name from `self.class.name`.
    + Add switch_disconnect function and print log message.

--- lib/hello_trema.rb
+++ lib/hello_trema.rb
@@ -1,10 +1,14 @@
 # Hello World!
 class HelloTrema < Trema::Controller
   def start(_args)
-    logger.info 'Trema started.'
+    logger.info "Hi! from " + self.class.name
   end
 
   def switch_ready(datapath_id)
     logger.info "Hello #{datapath_id.to_hex}!"
+  end
+
+  def switch_disconnected(dpid)
+    logger.info 'Stop ' + dpid.to_hex + '!'
   end
 end
